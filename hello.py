c9df9679c83275fa2ee5938a7eab37bff44f6615
rVmishima <s3823038@students.fscj.edu>
2025-08-19 16:59:31 -0400

    Added new message

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 8/19/2025
 # A simple Python program to print a message!
 
+# Ramiro Valdivia
+# 8/19/2025
+# Added new message... and spelled my name correctly this time
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
